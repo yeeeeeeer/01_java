bedc0bb69ca853d15ebb78f5890799600ba64e8a
yerin <greyblanket@naver.com>
2024-05-10 14:15:34 +0900

    0510

--- chap02-operator/src/main/java/com/ohgiraffers/section05/logical/Application01.java
+++ chap02-operator/src/main/java/com/ohgiraffers/section05/logical/Application01.java
@@ -35,5 +35,15 @@ public class Application01 {
         int d = 40;
 
         System.out.println("----------------------------");
+
+        System.out.println("a가 b보다 작으면서 c가 d보다 작은지 확인 : " + ((a < b) && (c < d)));
+        System.out.println("a가 b보다 작으면서 c가 d보다 큰지 확인 : " + ((a < b) && (c > d)));
+        System.out.println("a가 b보다 크면서 c가 d보다 작은지 확인 : " + ((a > b) && (c < d)));
+        System.out.println("a가 b보다 크면서 c가 d보다 큰지 확인 : " + ((a > b) && (c > d)));
+
+        System.out.println("a가 b보다 작으면서 c가 d보다 작은지 확인 : " + ((a < b) || (c < d)));
+        System.out.println("a가 b보다 작으면서 c가 d보다 큰지 확인 : " + ((a < b) || (c > d)));
+        System.out.println("a가 b보다 크면서 c가 d보다 작은지 확인 : " + ((a > b) || (c < d)));
+        System.out.println("a가 b보다 크면서 c가 d보다 큰지 확인 : " + ((a > b) || (c > d)));
     }
 }
