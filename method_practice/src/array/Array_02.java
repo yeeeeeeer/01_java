f82de4e2c471df87fbdda37ab2c820bb4efd8a57
yerin <greyblanket@naver.com>
2024-05-23 17:20:00 +0900

    0523

--- method_practice/src/array/Array_02.java
+++ method_practice/src/array/Array_02.java
@@ -22,47 +22,57 @@ public class Array_02 {
         // 사용자 입력받는 Scanner scanner
         Scanner scanner = new Scanner(System.in);
 
+        // 사용자에게 상품 이름, 가격 입력받고 - 배열 저장
+        // 10개 제한
+        int max = 10;
+        String[] productNames = new String[max];
+        int[] productPrices = new int[max];
+
+        // 등록된 상품 갯수 변수
+        int productCount = 0;
+
         // 선택 제시
         while (true) {
             System.out.println("1. 상품등록 / 2. 상품목록 / 3. 종료");
             System.out.println("메뉴 선택: ");
             int choice = scanner.nextInt();
+            scanner.nextLine();
 
-            // 선택 시 해당한 기능 실행
-            switch (choice) {
-                case 1:
-                    // 사용자에게 상품 이름, 가격 입력받고 - 배열 저장
-                    String[] productNames = new String[10];
-                    int[] productPrices = new int[10];
-                    System.out.println("상품 이름 입력: ");
-                    for (int i = 0; i < productNames.length; i++) {
-                        productNames[i] = scanner.next();
+            // 선택 -> 실행
+            // 1번 상품등록 - productNames 상품 이름, productPrices 가격 max(10개)까지 받기 -> 배열
+            if (choice == 1) {
+                if (productCount >= max) {
+                    System.out.println(" 더이상 상품을 등록할 수 없습니다. ");
+                    continue;
 
-                        System.out.println("상품 가격 입력: ");
-                        for (int j = 0; j < productPrices.length; j++) {
-                            productPrices[j] = scanner.nextInt();
-                        }
-                    }
+                }
+                System.out.println("상품 이름 입력 : ");
+                productNames[productCount] = scanner.nextLine();
 
-                    System.out.println("더이상 상품을 등록할 수 없습니다.");
-                    break;
+                System.out.println("상품 가격 입력 : ");
+                productPrices[productCount] = scanner.nextInt();
 
-                case 2:
-                    // 배열 저장된 모든 상품 출력
+                productCount++;
 
 
+            } else if (choice == 2) {
+                if (productCount == 0) {
+                    System.out.println("등록된 상품이 없습니다~");
+                    continue;
+                }
+                for (int i = 0; i < productCount; i++) {
+                    System.out.println(productNames[i] + " : " + productPrices[i] + " 원");
+                }
+
+
+            } else if (choice == 3) {
+                System.out.println("프로그램 종료");
+                break;
+
+
+            } else {
+                System.out.println("잘못된 메뉴를 선택했습니다");
             }
-
-
         }
-        // 상품 이름 저장 배열 String[] productNames
-//        String[]
-
-        // 상품 가격 저장 배열 int[] productPrices
-
-
-        // 등록된 상품 갯수 저장 변수 int productCount
-
-
     }
 }
